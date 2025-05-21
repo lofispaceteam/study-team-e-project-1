16cb99fc5506503964c0113efd2136a95b5cc6f5
Maxim Samusenko <maximsamusen@gmail.com>
2025-05-21 08:36:34 +0500

    Added Header, Quote components and changed styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Quote from "./components/Quote";
 
 function App() {
   return (
     <>
       <div className="flex flex-col h-screen justify-center items-center text-center bg-gray-100">
-        <main className="mb-auto w-[100%] flex-grow bg-gray-500">
-          <h1 className="text-5xl flex-grow text-green-500">Test title</h1>
+        <Header />
+        <main className="flex-grow w-full bg-gray-600">
+          <div className="max-w-4xl mx-auto px-4 py-8">
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl text-white text-center mb-8">
+              Random quote:
+            </h1>
+            <Quote
+              id={1}
+              text="Цитата дня — самая важная вещь на свете."
+              author="Марк Твен"
+              writingYear={1876}
+            />
+          </div>
         </main>
         <Footer />
       </div>
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="flex w-[100%] justify-center items-center text-center bg-gray-900 text-white py-6">
       <div className="container mt-auto px-4">
-        <p className="text-1xl font-bold ">© LofiSpace – QuotesApp – {year}</p>
+        <p className="text-1xl font-bold ">© LofiSpace – QuotesApp – {year}.</p>
       </div>
     </footer>
   );
