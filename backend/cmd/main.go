7c7ef67b9eb5abeca66d8ae6f2afeed0e2126752
krakovlev <sizov.lewa.06@gmail.com>
2025-06-09 19:56:22 +0500

    Create main.go

---
+++ backend/cmd/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"RandomQuote/internal/handlers"
+	"RandomQuote/internal/models"
+	"RandomQuote/internal/repositories"
+	"RandomQuote/internal/routes"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	fmt.Println("DB_USER:", os.Getenv("DB_USER"))
+	fmt.Println("DB_PASSWORD:", os.Getenv("DB_PASSWORD"))
+	fmt.Println("DB_NAME:", os.Getenv("DB_NAME"))
+	fmt.Println("DB_HOST:", os.Getenv("DB_HOST"))
+	fmt.Println("DB_PORT:", os.Getenv("DB_PORT"))
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to the DB")
+	}
+
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+
+	db.AutoMigrate(&models.Quote{})
+
+	quoteRepo := repositories.NewQuoteRepository(db)
+	quoteHandler := handlers.NewQuoteHandler(quoteRepo)
+
+	r := gin.Default()
+	routes.SetupRoutes(r, quoteHandler)
+
+	port := os.Getenv("PORT")
+	log.Printf("Server is running on port %s . . .", port)
+	if err := r.Run(":" + port); err != nil {
+		log.Fatal("Error starting the server")
+	}
+}
